37097cd44af282ec36e5dc1adb6249ff4306707c
Corcholog <loguerciogiorgioivan@gmail.com>
2025-05-02 19:58:28 -0300

    endpoint (functions) for frame predictions in tracker.py

--- tracker/main.py
+++ tracker/main.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import time
 from ultralytics import YOLO
 from track import SimpleTracker
 import time
@@ -22,7 +23,7 @@ class FrameProcessor:
             self.accumulator -= self.target_time
             return True
         return False
-#
+
 
 '''
 def should_process_frame (frame_index, video_fps, target_fps):
@@ -32,10 +33,18 @@ def should_process_frame (frame_index, video_fps, target_fps):
     return frame_index % frame_interval == 0
 '''
 
-def main (video_path, target_fps=15):
+
+    
+
+def main (video_path, target_fps=None):
     cap = cv2.VideoCapture(video_path)
     video_fps = cap.get(cv2.CAP_PROP_FPS)
+    if target_fps is None:
+        target_fps = video_fps
+    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    video_length = total_frames / video_fps
     print(video_fps)
+
 
     frame_processor = FrameProcessor(video_fps,target_fps)
 
@@ -45,13 +54,22 @@ def main (video_path, target_fps=15):
     cap = cv2.VideoCapture(video_path)
     processed_frames = 0
     start_time = time.time()
+
+    # Métricas de tiempo
+    processing_times = []
+    detection_times = []
+    tracking_times = []
+    start_processing_time = time.time()
     while cap.isOpened():
         ret, frame = cap.read()
         if not ret:
             break
             
         if frame_processor.should_process():
+            frame_start_time = time.time()
             processed_frames += 1
+            # --- Detección (YOLO) ---
+            det_start = time.time()
             results = model(frame)[0]
             detections = []
 
@@ -64,8 +82,13 @@ def main (video_path, target_fps=15):
                     if cropped.size == 0:
                         continue
                     detections.append(([x1, y1, x2, y2]))
+                    
+            det_end = time.time()
 
+            # --- Tracking ---
+            track_start = time.time()
             tracks = tracker.update(detections)
+            track_end = time.time()
 
             for track in tracks:
                 x1, y1, x2, y2 = track.bbox
@@ -76,7 +99,17 @@ def main (video_path, target_fps=15):
                 label = f"ID {track_id}"
                 cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 200, 100), 2)
                 cv2.putText(frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 200, 100), 2)
+            
+            frame_end_time = time.time()
+            elapsed_frame_time = frame_end_time - frame_start_time
 
+            # Guardar métricas
+            processing_times.append(elapsed_frame_time)
+            detection_times.append(det_end - det_start)
+            tracking_times.append(track_end - track_start)
+
+            cv2.imshow("Tracker", frame)
+            
         # Mostrar FPS cada segundo
         elapsed = time.time() - start_time
         if elapsed >= 1.0:  # Cada 1 segundo
@@ -84,15 +117,38 @@ def main (video_path, target_fps=15):
             print(f"[INFO] FPS por segundo: {fps:.2f} ")
             processed_frames = 0  # Reseteamos los frames procesados en este segundo
             start_time = time.time()  # Reiniciamos el tiempo
-        
-        cv2.imshow("Tracker", frame)
-        if cv2.waitKey(1) & 0xFF == ord("q"):
-            break
 
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+                break
+        
     cap.release()
     cv2.destroyAllWindows()
 
+    # --- Resultados ---
+    if processing_times:
+        avg_time = np.mean(processing_times)
+        worst_time = np.max(processing_times)
+        best_time = np.min(processing_times)
+        total_processing_time = time.time() - start_processing_time
 
-video_path = 'videotracker.mp4'
+        print(f"FPS originales del video: {video_fps}")
+        print(f"\nMétricas de procesamiento por frame ( Limitado a {target_fps} fps):")
+        print(f"Tiempo promedio: {avg_time:.4f} segundos ({1/avg_time:.2f} FPS aprox)")
+        print(f"Peor caso: {worst_time:.4f} segundos ({1/worst_time:.2f} FPS)")
+        print(f"Mejor caso: {best_time:.4f} segundos ({1/best_time:.2f} FPS)\n")
 
-main(video_path)
+        
+        print(f"Duración total del video: {video_length:.2f} segundos ({total_frames} frames)")
+        print(f"\nTiempo total de procesamiento: {total_processing_time:.2f} segundos")
+        print(f"FPS promedio durante el procesamiento: {total_frames / total_processing_time:.2f} FPS")
+
+        print(f"\nMétricas de tiempo (detectar / seguir):")
+        print(f"Tiempo promedio de detección: {np.mean(detection_times):.4f} segundos")
+        print(f"Tiempo promedio de seguimiento: {np.mean(tracking_times):.4f} segundos")
+    else:
+        print("No se procesó ningún frame.")
+
+
+video_path = r'tracker\park.mp4'
+
+main(video_path, 20)
